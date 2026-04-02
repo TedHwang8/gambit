43ae75db3b9211309483e76d0ce6d6b71240372b
Changil Hwang <changilhwang@Changils-Air.attlocal.net>
2026-04-01 17:09:46 -0700

    Explicitly bootstrap Stockfish worker

--- vendor/stockfish-worker.js
+++ vendor/stockfish-worker.js
@@ -1,1 +1,11 @@
 importScripts('./stockfish.js');
+
+const engine = STOCKFISH('./stockfish.wasm');
+
+self.onmessage = (event) => {
+  engine.postMessage(event.data);
+};
+
+engine.onmessage = (line) => {
+  self.postMessage(line);
+};
